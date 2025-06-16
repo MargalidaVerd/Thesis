25214c714c77ec6c01feee9556f13235e463aef6
marga <mverdjulia@gmail.com>
2025-06-16 16:02:21 +0200

    Defined new ACF and PACF functions.

---
+++ acf_pacf_ar1_code.R
@@ -0,0 +1,63 @@
+set.seed(123)
+
+phi_positive <- 0.8
+phi_negative <- -0.8
+
+sim_positive <- arima.sim(model = list(ar = phi_positive), n = 100)
+sim_negative <- arima.sim(model = list(ar = phi_negative), n = 100)
+
+custom_acf_plot <- function(ts_data, max_lag = 25) {
+  acf_vals <- acf(ts_data, plot = FALSE)
+  acf_df <- with(acf_vals, data.frame(
+    Lag = lag,
+    ACF_value = acf
+  ))
+  
+  conf <- qnorm(0.975) / sqrt(length(ts_data))
+  acf_df$UCL <- conf
+  acf_df$LCL <- -conf
+  
+  ggplot(acf_df, aes(x = Lag, y = ACF_value)) +
+    geom_hline(yintercept = 0, color = "#1f77b4") +
+    geom_ribbon(aes(ymin = LCL, ymax = UCL), fill = "#1f77b4", alpha = 0.2) +
+    geom_segment(aes(xend = Lag, yend = 0), color = "#1f77b4") +
+    geom_point(size = 1.5, color = "#1f77b4") +
+    coord_cartesian(ylim = c(-0.5, 1.01)) +
+    scale_y_continuous(breaks = seq(-0.5, 1.01, by = 0.25)) +
+    scale_x_continuous(breaks = seq(0, max_lag, by = 2)) +
+    theme_minimal() +
+    theme(panel.grid = element_blank()) +
+    labs(title = "", x = "Lag", y = "Value")
+}
+
+custom_pacf_plot <- function(ts_data, max_lag = 25) {
+  pacf_vals <- pacf(ts_data, plot = FALSE)
+  pacf_df <- with(pacf_vals, data.frame(
+    Lag = lag,
+    PACF_value = acf
+  ))
+  
+  conf <- qnorm(0.975) / sqrt(length(ts_data))
+  pacf_df$UCL <- conf
+  pacf_df$LCL <- -conf
+  
+  ggplot(pacf_df, aes(x = Lag, y = PACF_value)) +
+    geom_hline(yintercept = 0, color = "#1f77b4") +
+    geom_ribbon(aes(ymin = LCL, ymax = UCL), fill = "#1f77b4", alpha = 0.2) +
+    geom_segment(aes(xend = Lag, yend = 0), color = "#1f77b4") +
+    geom_point(size = 1.5, color = "#1f77b4") +
+    coord_cartesian(ylim = c(-0.7, 1.01)) +
+    scale_y_continuous(breaks = seq(-0.5, 1.01, by = 0.25)) +
+    scale_x_continuous(breaks = seq(0, max_lag, by = 2)) +
+    theme_minimal() +
+    theme(panel.grid = element_blank()) +
+    labs(title = "", x = "Lag", y = "Value")
+}
+
+
+custom_acf_plot(sim_positive)
+custom_pacf_plot(sim_positive)
+
+custom_acf_plot(sim_negative)
+custom_pacf_plot(sim_negative)
+
--- white_noise_acf_code.R
+++ white_noise_acf_code.R
@@ -1,12 +1,59 @@
-set.seed(42)
+set.seed(16102001)
 
 wn <- rnorm(200, mean = 0, sd = 1)
 
-ts.plot(wn, xlab = "Time")
+plot(wn, type = "l", col = "#1f77b4", lwd = 1,
+     main = "",
+     xlab = "Time", ylab = "Value")
 
-abline(h = 0, col = "red", lty = 2)
+y_ts <- ts(wn)
+acf_vals <- acf(y_ts, plot = FALSE)
+pacf_vals <- pacf(y_ts, plot = FALSE)
 
-acf(wn, main = "ACF")
 
-hist(wn, breaks = 20, main = "Histogram of White Noise", col = "lightblue", probability = TRUE)
-curve(dnorm(x, mean = mean(wn), sd = sd(wn)), add = TRUE, col = "red")
+acf_df <- with(acf_vals, data.frame(
+  Lag = lag,
+  ACF_value = acf
+))
+
+
+pacf_df <- with(pacf_vals, data.frame(
+  Lag = lag,
+  PACF_value = acf
+))
+
+confidence_interval <- qnorm((1 + 0.95)/2)/sqrt(length(y))
+
+acf_df$UCL <- conf
+acf_df$LCL <- -conf
+
+pacf_df$UCL <- conf
+pacf_df$LCL <- -conf
+
+acf_plot <- ggplot(acf_df, aes(x = Lag, y = ACF_value)) +
+  geom_hline(yintercept = 0, color = "#1f77b4") +
+  geom_ribbon(aes(ymin = LCL, ymax = UCL), fill = "#1f77b4", alpha = 0.2) +
+  geom_segment(aes(xend = Lag, yend = 0), color = "#1f77b4") +
+  geom_point(size = 1.5, color = "#1f77b4") +
+  coord_cartesian(ylim = c(-0.5, 1.01)) +
+  scale_y_continuous(breaks = seq(-0.5, 1.01, by = 0.25)) +
+  scale_x_continuous(breaks = seq(0,25, by = 2)) +
+  theme_minimal() +
+  theme(panel.grid = element_blank()) +
+  labs(title = "", x = "Lag", y = "Value")
+
+acf_plot
+
+pacf_plot <- ggplot(pacf_df, aes(x = Lag, y = PACF_value)) +
+  geom_hline(yintercept = 0, color = "#1f77b4") +
+  geom_ribbon(aes(ymin = LCL, ymax = UCL), fill = "#1f77b4", alpha = 0.2) +
+  geom_segment(aes(xend = Lag, yend = 0), color = "#1f77b4") +
+  geom_point(size = 1.5, color = "#1f77b4") +
+  coord_cartesian(ylim = c(-0.5, 1.01)) +
+  scale_y_continuous(breaks = seq(-0.5, 1.01, by = 0.25)) +
+  scale_x_continuous(breaks = seq(0,25, by = 2)) +
+  theme_minimal() +
+  theme(panel.grid = element_blank()) +
+  labs(title = "", x = "Lag", y = "Value")
+
+pacf_plot
